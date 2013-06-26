e121ad651308798e83b9f2da57426508a41edde7
Prabhu Das <prabhu.das@clogeny.com>
2013-06-26 14:55:47 +0530

    server create and delete functional test-cases.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'chef/knife/bootstrap'
 require 'chef/knife/winrm_base'
+require 'chef/knife/bootstrap_windows_winrm'
 require 'chef/knife/openstack_server_create'
 require 'chef/knife/openstack_server_delete'
